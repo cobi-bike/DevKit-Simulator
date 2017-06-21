d4ca63906b949d9d7a2d7896171db5fcc5d8d523
Camilo Roca <camilo.roca@cobi.bike>
2017-06-21 10:38:18 +0200

    CDK-21: monkey path event emitter lib to cache and intercept COBI spec messages

--- src/meta.js
+++ src/meta.js
@@ -3,5 +3,51 @@
 const containsCOBIjs = 'COBI !== null && COBI !== undefined'
 const emitStr = (path: string, value: any) => `COBI.__emitter.emit("${path}", ${JSON.stringify(value)})`
 
+/**
+ * this code should be injected to a webpage containing COBI.js in order
+ * to create an echo server for read/write messages
+ * It basically monkey patches the parts that are expected to be there
+ * when running on an iOS system, but that are not on a Chrome browser
+ */
+const fakeiOSWebkit =
+  `(function () {
+    if (!window.webkit) {
+      window.webkit = {
+        messageHandlers: {
+          cobiShell: {
+            cache: {},
+            postMessage: (msg) => {
+              switch (msg.action) {
+                case 'WRITE':
+                  window.webkit.messageHandlers.cobiShell.cache[msg.path] = msg.payload
+                  return setTimeout(() => COBI.__emitter.emit(msg.path, msg.payload),
+                                    2 * Math.random()) // 0 to 2 seconds. Fake asynchronisity
+                case 'READ':
+                  if (window.webkit.messageHandlers.cobiShell.cache[msg.path]) {
+                    setTimeout(() => COBI.__emitter.emit(msg.path, window.webkit.messageHandlers.cobiShell.cache[msg.path]),
+                               2 * Math.random()) // 0 to 2 seconds. Fake asynchronisity
+                  }
+                  return
+              }
+              throw new Error('wrong action passed: ' + msg.action)
+            }
+          }
+        }
+      }
+    }
+
+    // monkey path the emit function to hijack all cobi events
+    // and put them in the cache
+    var oldEmit = COBI.__emitter.emit
+    COBI.__emitter.emit = function () {
+      var event = arguments[0]
+      if (typeof event === 'string' && event.match(/^\\w+\\/\\w+$/)) {
+        window.webkit.messageHandlers.cobiShell.cache[event] = arguments[1]
+      }
+      oldEmit.apply(COBI.__emitter, arguments)
+    }
+  })()`
+
 module.exports.containsCOBIjs = containsCOBIjs
 module.exports.emitStr = emitStr
+module.exports.fakeiOSWebkit = fakeiOSWebkit
