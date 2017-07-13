24fe1d54e02519a4e4d55a74b40fdfda30789bdd
Camilo Roca <camilo.roca@cobi.bike>
2017-07-13 18:58:06 +0200

    cosmetic changes

--- src/math.js
+++ src/math.js
@@ -8,11 +8,11 @@ function computeDistance (lat1: number, lon1: number, lat2: number, lon2: number
   var R = 6371 // km
   var dLat = toRadians(lat2 - lat1)
   var dLon = toRadians(lon2 - lon1)
-  var lat1 = toRadians(lat1)
-  var lat2 = toRadians(lat2)
+  var rlat1 = toRadians(lat1)
+  var rlat2 = toRadians(lat2)
 
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+          Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(rlat1) * Math.cos(rlat2)
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
   var d = R * c
   return d
