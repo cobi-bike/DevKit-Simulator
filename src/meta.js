d295f6baa63164228ab8a08045f894ffefccbb35
Camilo Roca <camilo.roca@cobi.bike>
2017-07-17 15:58:56 +0200

    remove duplicated code

--- src/meta.js
+++ src/meta.js
@@ -4,53 +4,6 @@ const containsCOBIjs = 'COBI ? COBI.specVersion : null'
 const emitStr = (path: string, value: any) => `COBI.__emitter.emit("${path}", ${JSON.stringify(value)})`
 const fetch = (path: string) => `window.webkit.messageHandlers.cobiShell.cache['${path}']`
 
-/**
- * this code should be injected to a webpage containing COBI.js in order
- * to create an echo server for read/write messages
- * It basically monkey patches the parts that are expected to be there
- * when running on an iOS system, but that are not on a Chrome browser
- */
-const fakeiOSWebkit = `
-  (function () {
-    if (!window.webkit) {
-      window.webkit = {
-        messageHandlers: {
-          cobiAuth: {
-            postMessage: (authKey) => window.COBI.__authenticated({confirmed: true, apiKey: authKey})
-          },
-          cobiShell: {
-            cache: {},
-            postMessage: (msg) => {
-              switch (msg.action) {
-                case 'WRITE':
-                  window.webkit.messageHandlers.cobiShell.cache[msg.path] = msg.payload
-                  return setTimeout(() => COBI.__emitter.emit(msg.path, msg.payload),
-                                    2 * Math.random()) // 0 to 2 seconds. Fake asynchronisity
-                case 'READ':
-                  if (window.webkit.messageHandlers.cobiShell.cache[msg.path]) {
-                    setTimeout(() => COBI.__emitter.emit(msg.path, window.webkit.messageHandlers.cobiShell.cache[msg.path]),
-                               2 * Math.random()) // 0 to 2 seconds. Fake asynchronisity
-                  }
-                  return
-              }
-              throw new Error('wrong action passed: ' + msg.action)
-            }
-          }
-        }
-      }
-      // monkey path the emit function to hijack all cobi events
-      // and put them in the cache
-      var oldEmit = COBI.__emitter.emit
-      COBI.__emitter.emit = function () {
-        var event = arguments[0]
-        if (typeof event === 'string' && event.match(/^\\w+\\/\\w+$/)) {
-          window.webkit.messageHandlers.cobiShell.cache[event] = arguments[1]
-        }
-        oldEmit.apply(COBI.__emitter, arguments)
-      }
-    }
-  })()`
-
 const welcome = `
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -83,6 +36,5 @@ const welcome = `
 
 module.exports.containsCOBIjs = containsCOBIjs
 module.exports.emitStr = emitStr
-module.exports.fakeiOSWebkit = fakeiOSWebkit
 module.exports.welcome = welcome
 module.exports.fetch = fetch
